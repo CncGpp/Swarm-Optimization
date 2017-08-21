e475176213deb45d3bf269c6332bd6271cb465a1
CncGpp <giuseppecianci.95@gmail.com>
2017-08-21 16:49:11 +0200

    Aggiunto il JavaDoc

--- src/model/entity/Colony.java
+++ src/model/entity/Colony.java
@@ -1,24 +1,41 @@
 package model.entity;
 
 
+import model.AGame;
 import model.map.AMap;
 import util.Coord;
 
+/**
+ * Implementazione concreta della classe {@code AColony}
+ * @see AColony
+ */
 public class Colony extends AColony{
 
+	/**
+	 * Istanzia una nuova colonia di bot.
+	 *
+	 * @param map l'istanza della mappa a cui la colonia deve appartenere.
+	 * @param botCount il numero di bot che devono far parte della colonia.
+	 * @param start l'entita' {@code AStart} da dove la colonia dovra' cominciare.
+	 *
+	 * @see AGame
+	 * @see AStart
+	 */
 	public Colony(final AMap map, final int botCount, final AStart start) {
 		super(map, botCount, start);
 	}
 
-	//Factory method
+	/* (non-Javadoc)
+	 * @see model.entity.AColony#makeBot(util.Coord)
+	 */
 	@Override
 	protected ABot makeBot(final Coord startPosition){ return new Bot(this, startPosition); }
 
-	//Definisce l'ordine e in che modo di devono muovere i bot
-	//In questo caso tutti insieme
+	/* (non-Javadoc)
+	 * @see model.entity.AColony#moveBot()
+	 * Tutti i bot si muovono ad ogni cambio di stato
+	 */
 	@Override
-	protected void moveBot() {
-		for(ABot b : this.getBots()) b.move();
-	}
+	protected void moveBot() { for(ABot b : this.getBots()) b.move(); }
 
 }
