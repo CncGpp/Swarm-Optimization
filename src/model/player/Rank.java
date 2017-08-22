b099c39f8e0725a7c9fdfdae90b6ea9e31cc9ef0
CncGpp <giuseppecianci.95@gmail.com>
2017-08-22 10:05:18 +0200

    Aggiunto JavaDoc

--- src/model/player/Rank.java
+++ src/model/player/Rank.java
@@ -11,12 +11,24 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.SortedList;
 
+/**
+ * la classe {@code Rank} gestisce i punteggi dei giocatori.
+ * <p> I punteggi sono memorizzati su file quindi la classe si occupa della lettura e scrittura su tale file.
+ * In oltre la classe fornisce un punto di accesso alla classifica, infatti tutti gli attributi sono statici </p>
+ */
 public class Rank {
-	private File f;
-	private ObservableList<PlayerScore> observableList_rank;
-	private SortedList<PlayerScore> sortedList;
+	private static File f;
+	private static ObservableList<PlayerScore> observableList_rank;
+	private static SortedList<PlayerScore> sortedList;
 
+	/**
+	 * Crea una nuova istanza di Rank
+	 * <p> La prima istanza creata leggerà la classifica dal file riempiendo la lista dei punteggi </p>
+	 * @param path the path
+	 */
 	public Rank(String path){
+		if(observableList_rank != null) return;											//La lista va caricata solo la prima volta
+
 		f = new File(path);
 		if(!f.exists())
 			try { f.createNewFile(); } catch (IOException e1) {e1.printStackTrace();}
@@ -36,6 +48,7 @@ public class Rank {
 			observableList_rank.add(new PlayerScore(fs.next(), fs.next(), fs.nextLong()));
 		} catch (FileNotFoundException e) { e.printStackTrace(); }
 	}
+
 
 	public void submitScore(final PlayerScore ps){
 		submitScore(ps.getName(), ps.getSurname(), ps.getTime());
